5efe8abf28dd0ed559080f7ac3ef5b91e87a2371
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-03-27 16:24:42 +0100

    Add a consistency check on phash2/1.

--- maps_iso_eqc.erl
+++ maps_iso_eqc.erl
@@ -79,6 +79,26 @@ prop_merge() ->
             Merged)
       end).
 
+%% phash2/1 consistency
+%%
+%% phash2/1 is consistent if elements which compare equal have the same hash value.
+prop_phash2_consistent() ->
+    ?FORALL(L, list(maps_eqc:map_term()),
+        begin
+            consistent(eqc_lib:sort(L))
+        end).
+        
+%% A sorted list is consistent if equal elements next to each other are consistent
+consistent([]) -> true;
+consistent([_X]) -> true;
+consistent([X, Y | XYs]) when X =:= Y ->
+    case erlang:phash2(X) =:= erlang:phash2(Y) of
+        true -> consistent([Y | XYs]);
+        false -> {X, '/=', Y}
+    end;
+consistent([_X | XYs]) -> consistent(XYs).
+
+
 %% Deduplicate a list
 is_key(_K, []) -> false;
 is_key(K, [{K, _} | _]) -> true;
