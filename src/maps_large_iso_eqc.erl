a5ede321fb41b0b62c7b91713c5b11159dc13069
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-04-02 16:08:48 +0200

    Rather than generating integers, generate map keys and values from maps_eqc.

--- src/maps_large_iso_eqc.erl
+++ src/maps_large_iso_eqc.erl
@@ -13,16 +13,16 @@ rand_seed(Alg) ->
     ?LET(Seed, rand_seed(),
         rand:seed_s(Alg, Seed)).
         
-large_map(_N, 0, _RandState, Acc) -> Acc;
-large_map(N, K, RandState, Acc) ->
-    {Key, RS1} = rand:uniform_s(N, RandState),
-    {Value, RS2} = rand:uniform_s(N, RS1),
-    large_map(N, K-1, RS2, [{Key, Value} | Acc]).
+large_map(_N, 0, _FK, _FV, _RandState, Acc) -> Acc;
+large_map(N, K, FK, FV, RandState, Acc) ->
+    {KK, RS1} = rand:uniform_s(N, RandState),
+    {KV, RS2} = rand:uniform_s(N, RS1),
+    large_map(N, K-1, FK, FV, RS2, [{FK(KK), FV(KV)} | Acc]).
 
 prop_lm_iso_fg() ->
-    ?FORALL(RS, rand_seed(exs64),
+    ?FORALL({RS, FK, FV}, {rand_seed(exs64), function1(maps_eqc:map_key()), function1(maps_eqc:map_value())},
        begin
-          Map = maps:from_list(large_map(65536*65536, 25000, RS, [])),
+          Map = maps:from_list(large_map(65536*65536, 25000, FK, FV, RS, [])),
           L = maps:to_list(Map),
           Map2 = maps:from_list(L),
           Map =:= Map2
