5731f26a3d8b6697311a98ad2d3eeb51a9fd3ca6
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2015-04-02 15:15:36 +0200

    Use random Alg exs64 rather than as183.

--- src/maps_large_iso_eqc.erl
+++ src/maps_large_iso_eqc.erl
@@ -11,7 +11,7 @@ rand_seed() ->
     
 rand_seed(Alg) ->
     ?LET(Seed, rand_seed(),
-        rand:seed_s({Alg, Seed})).
+        rand:seed_s(Alg, Seed)).
         
 large_map(_N, 0, _RandState, Acc) -> Acc;
 large_map(N, K, RandState, Acc) ->
@@ -20,7 +20,7 @@ large_map(N, K, RandState, Acc) ->
     large_map(N, K-1, RS2, [{Key, Value} | Acc]).
 
 prop_lm_iso_fg() ->
-    ?FORALL(RS, rand_seed(as183),
+    ?FORALL(RS, rand_seed(exs64),
        begin
           Map = maps:from_list(large_map(65536*65536, 25000, RS, [])),
           L = maps:to_list(Map),
